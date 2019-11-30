251ad328f537fd48fd9145a0c4f3bee5bcab6aaa
Jakub Adamczyk <jkk.adamczyk@gmail.com>
2019-11-30 12:10:48 +0100

    prepare fragments and activities to use in the app

---
+++ app/src/main/java/com/example/weatherprovider/CitySearchActivity.kt
@@ -0,0 +1,12 @@
+package com.example.weatherprovider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CitySearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_city_search)
+    }
+}
---
+++ app/src/main/java/com/example/weatherprovider/fragments/WeatherForecastViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.weatherprovider.fragments
+
+import androidx.lifecycle.ViewModel
+
+class WeatherForecastViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
