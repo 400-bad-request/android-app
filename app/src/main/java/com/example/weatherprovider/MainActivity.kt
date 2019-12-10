16646febf1164b47d2c91bf80df8be1456517147
SkalskiP <piotr.skalski92@gmail.com>
2019-12-10 23:12:59 +0100

    CitySearchActivity intent added

--- app/src/main/java/com/example/weatherprovider/MainActivity.kt
+++ app/src/main/java/com/example/weatherprovider/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.example.weatherprovider
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import androidx.viewpager.widget.ViewPager
-import com.example.weatherprovider.forecast.ForecastViewModel
-import com.example.weatherprovider.model.ForecastLocation
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +15,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewPager = findViewById(R.id.viewPager)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_view_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val id = item!!.itemId
+        if (id == R.id.add_button) {
+            val intent = Intent(this, CitySearchActivity::class.java).apply {}
+            startActivity(intent)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
  
