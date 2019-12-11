9308b67f215aaa7ca27ebb78faaa13650b870df3
SkalskiP <piotr.skalski92@gmail.com>
2019-12-11 16:00:53 +0100

    Detail update

--- app/src/main/java/com/example/weatherprovider/location/LocationFragment.kt
+++ app/src/main/java/com/example/weatherprovider/location/LocationFragment.kt
@@ -1,21 +1,33 @@
 package com.example.weatherprovider.location
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.AsyncTask
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
 import com.example.weatherprovider.R
 import com.example.weatherprovider.api.WeatherSearchAPI
 import com.example.weatherprovider.api.model.CitySearchResult
 import com.example.weatherprovider.api.model.WeatherSearchResult
+import java.net.URL
 
 
 class LocationFragment : Fragment() {
     // Store instance variables
     private var cityName: String? = null
     private var woeid: Int? = 0
+    private var weatherData: WeatherSearchResult? = null
+
+    private lateinit var cityNameLabel: TextView
+    private lateinit var cityParentNameLabel: TextView
+    private lateinit var weatherImageAvatar: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,13 +45,48 @@ class LocationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_location_forecast, container, false)
-        val woeidLabel = view.findViewById(R.id.woeid_text) as TextView
-        woeidLabel.text = woeid.toString()
+        cityNameLabel = view.findViewById(R.id.city_name) as TextView
+        cityParentNameLabel = view.findViewById(R.id.city_parent_name) as TextView
+        weatherImageAvatar = view.findViewById(R.id.weather_image_avatar) as ImageView
+
+        if (weatherData !== null) {
+            applyDataOnView(weatherData)
+        }
+
         return view
     }
 
     val onSuccessfulSearchCallback: (data: WeatherSearchResult) -> Unit = { data ->
-        println(data)
+        weatherData = data
+        applyDataOnView(data)
+    }
+
+    val applyDataOnView: (data: WeatherSearchResult?) -> Unit = { data ->
+        cityNameLabel.text = data?.title
+        cityParentNameLabel.text = data?.parent?.title
+
+        val currentWeather = data?.consolidated_weather?.first()
+        DownLoadImageTask(weatherImageAvatar).execute("https://www.metaweather.com/static/img/weather/png/${currentWeather?.weather_state_abbr}.png")
+    }
+
+    private class DownLoadImageTask(internal val imageView: ImageView) : AsyncTask<String, Void, Bitmap?>() {
+        override fun doInBackground(vararg urls: String): Bitmap? {
+            val urlOfImage = urls[0]
+            return try {
+                val inputStream = URL(urlOfImage).openStream()
+                BitmapFactory.decodeStream(inputStream)
+            } catch (e: Exception) { // Catch the download exception
+                e.printStackTrace()
+                null
+            }
+        }
+        override fun onPostExecute(result: Bitmap?) {
+            if(result!=null){
+                imageView.setImageBitmap(result)
+            } else {
+                Toast.makeText(imageView.context,"Error downloading",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     companion object {
